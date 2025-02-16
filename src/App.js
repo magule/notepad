96203bcdd3094b528d5d2e2606c35e4e0061cb4a
magule <furkankalaycioglu99@gmail.com>
2025-02-16 18:03:11 +0300

    analytics

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import './fonts.css';
 import LegalPad from './components/LegalPad';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <LegalPad />
+      <Analytics />
     </div>
   );
 }
